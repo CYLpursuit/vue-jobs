7399e62a57ba46d9d3b4639dc2037037badab111
tiantong <tiantong@webuy.ai>
2020-11-19 15:41:48 +0800

    visibilitychange API　

---
+++ jobs/src/views/visibilityChange.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="time"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    document.getElementById('time').innerHTML = new Date();
+    document.addEventListener("visibilitychange", function() {
+      console.log( document.visibilityState );
+      console.log(document.hidden);
+    });
+  }
+}
+</script>
